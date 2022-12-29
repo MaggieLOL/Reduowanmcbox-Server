ba342f0b44523086f3dc7558213c6e5095017d72
Maggie <96947247+MaggieLOL@users.noreply.github.com>
2022-12-29 10:34:04 +0800

    Delete view.py

--- view.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-import os
-def craftRoom(request):
-    with open ("room.flag","w") as f:
-        f.write(request.POST["flag"])
-    return HttpResponse("ok")
-def quitRoom(request):  
-    os.system("rm -rf room.flag")
-    return HttpResponse("ok")
